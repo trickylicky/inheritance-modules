82efca7d4693434352d4c76dd708fb410c506d2f
Tricky Licky <wangombevictor5@gmail.com>
2022-11-01 12:41:26 +0300

    complete assignment

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative 'fancyDance'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+end
